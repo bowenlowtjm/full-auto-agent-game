bb2fffe3bff7c4b884cc9cd71016614da0ce6c00
Bowen Low <bowen.low.88@gmail.com>
2026-06-05 13:50:53 +0800

    fix: Unity.ugui asmdef refs + InputSystem.Mouse using directive

--- Assets/_Game/Scripts/GestureRecognizer.cs
+++ Assets/_Game/Scripts/GestureRecognizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
